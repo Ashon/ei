98b20e33aaf1f45af95cf4a332b42138e1aaf0c7
Ashon <ashon8813@gmail.com>
2023-01-06 15:06:32 +0900

    Beautify serializer fns

--- ei/core/field_serializers.py
+++ ei/core/field_serializers.py
@@ -7,13 +7,17 @@ indented_json = partial(json.dumps, indent=2)
 
 
 def serialize_tags(aws_tags):
-    return '\n'.join(
-        f'{t["Key"]} = {t["Value"]}' for t in aws_tags
-    )
+    return '\n'.join([
+        f'{t["Key"]} = {t["Value"]}'
+        for t in aws_tags
+    ])
 
 
 def serialize_dict_list(dict_list):
-    return '\n'.join(
-        '\n'.join([f'{key} = {value}' for key, value in obj.items()])
+    return '\n'.join([
+        '\n'.join([
+            f'{key} = {value}'
+            for key, value in obj.items()
+        ])
         for obj in dict_list
-    )
+    ])
