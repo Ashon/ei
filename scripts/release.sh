ea8d6b23b1390dbd4ec3ace209f024630f3752c2
Ashon <ashon8813@gmail.com>
2023-01-15 20:31:20 +0900

    Add release script, change release to publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,14 +2,48 @@
 
 set -euo pipefail
 
-scriptPath=$(dirname $0)
-. $scriptPath/_utils.sh
+option=$1
 
-log '[1/3] Install dependencies'
-pip install -e '.[devel]'
+# Read version
+VERSION_PATH="ei/__about__.py"
+VERSION_STR=$(python -c 'from ei.__about__ import VERSION; print(VERSION)')
+VERSION=(${VERSION_STR//./ })
 
-log "[2/3] Build packages"
-hatch build
+MAJOR="${VERSION[0]}"
+MINOR="${VERSION[1]}"
+PATCH="${VERSION[2]}"
 
-log "[3/3] Pubilsh to pypi"
-hatch publish
+echo "Make new release from $VERSION_STR (up $option version)"
+
+if [ $option = 'major' ]; then
+  MAJOR=$(($MAJOR + 1))
+  MINOR=0
+  PATCH=0
+
+elif [ $option = 'minor' ]; then
+  MINOR=$(($MINOR + 1))
+  PATCH=0
+
+elif [ $option = 'patch' ]; then
+  PATCH=$(($PATCH + 1))
+
+else
+  exit 1
+
+fi
+
+NEW_VERSION="$MAJOR.$MINOR.$PATCH"
+echo "New release is created $NEW_VERSION"
+
+# Write new version, commit
+echo "VERSION = '${NEW_VERSION}'" > $VERSION_PATH
+git add $VERSION_PATH
+git commit -am "Release $NEW_VERSION"
+git tag $NEW_VERSION
+
+echo "New release tag is created $NEW_VERSION"
+echo "Push to main repository (tag: $NEW_VERSION)"
+git push origin master $NEW_VERSION
+echo "Done"
+
+exit 0
