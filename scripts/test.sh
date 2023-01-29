ae082b8652dc430477d8da9bf35487408c987d3f
Ashon <ashon8813@gmail.com>
2023-01-29 22:19:52 +0900

    Seperate ci by python version

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,17 +2,27 @@
 
 set -euo pipefail
 
+python_version=${1:-""}
+
 scriptPath=$(dirname $0)
 . $scriptPath/_utils.sh
+
+if [ $python_version ]; then
+  py_version_arg="+py=${python_version}"
+else
+  py_version_arg=""
+fi
+
+log "Run Test ${python_version} ${py_version_arg}"
 
 log '[1/4] Install dependencies'
 pip install -e '.[devel]'
 
 log "[2/4] Lint"
-hatch run test:lint
+hatch run ${py_version_arg} test:lint
 
 log "[3/4] TypeChecking"
-hatch run test:typecheck
+hatch run ${py_version_arg} test:typecheck
 
 log "[4/4] UnitTest"
-hatch run test:test
+hatch run ${py_version_arg} test:test
