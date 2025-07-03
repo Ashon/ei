5cbb3c92660143f1202ff24074233297745431ee
Ashon Lee <ashon.lee@daangn.com>
2025-07-03 23:33:44 +0900

    Update action

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,18 +2,10 @@
 
 set -euo pipefail
 
-python_version=${1:-""}
-
 scriptPath=$(dirname $0)
 . $scriptPath/_utils.sh
 
-if [ $python_version ]; then
-  py_version_arg="+py=${python_version}"
-else
-  py_version_arg=""
-fi
-
-log "Run Test ${python_version} ${py_version_arg}"
+log "Run Test"
 log "- on python version: $(uv run python --version)"
 
 log '[1/4] Install dependencies'
